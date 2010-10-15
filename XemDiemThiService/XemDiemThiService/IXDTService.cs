e18a2bd202d56750a8cc94e24a9454926bb11be8
nvcuong.infotech@yahoo.com <nvcuong.infotech@yahoo.com@0f7d6440-41b5-22ce-4b88-147af5e17b3c>
2010-10-15 17:08:02 +0000

    - Code phía WebService (WCF) - Công cụ sử dụng: Visual Studio 2010

---
+++ XemDiemThiService/XemDiemThiService/IXDTService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+using XemDiemThiService.DAL;
+using XemDiemThiService.Model;
+
+namespace XemDiemThiService
+{
+    [ServiceContract]
+    public interface IXDTService
+    {
+        [OperationContract]
+        ThiSinhDetails GetThiSinhDetails(string soBaoDanh);
+
+        [OperationContract]
+        ThiSinhDetails[] GetTop100ThiSinhDetailsByMaTruong(string maTruong);
+
+        [OperationContract]
+        TruongDetails[] GetAllTruong();
+    }
+}
---
+++ XemDiemThiService/XemDiemThiService/Model/TruongDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace XemDiemThiService.Model
+{
+    [DataContract]
+    public class TruongDetails
+    {
+        [DataMember]
+        public string MaTruong { get; set; }
+
+        [DataMember]
+        public string TenTruong { get; set; }
+    }
+}
