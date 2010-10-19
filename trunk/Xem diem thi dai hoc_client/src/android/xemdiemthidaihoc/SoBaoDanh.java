8f98e527869daa8ff74495d6847ca6f1834a0648
minhnguyen.gtvt <minhnguyen.gtvt@0f7d6440-41b5-22ce-4b88-147af5e17b3c>
2010-10-19 17:13:00 +0000

    Đây là phần Client viết cho android. Đả hoàn thành một phần giao diện. Phần xem điểm thi bằng cách nhập số báo danh đả làm xong. Bạn cương hảy test thử bằng service của mình . Nếu thấy lổi kết nối hảy vào sobaodanh/ChiTietThiSinh.java sửa lại các thông số kết nối. Nếu vẩn lổi thì hảy reply lại cho mình .

---
+++ trunk/Xem diem thi dai hoc_client/src/android/xemdiemthidaihoc/SoBaoDanh.java
@@ -0,0 +1,37 @@
+package android.xemdiemthidaihoc;
+
+
+
+import sobaodanh.ChiTietThiSinh;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+
+public class SoBaoDanh extends Activity implements OnClickListener {
+    /** Called when the activity is first created. */
+	private static final int chi_tiet = 1;
+	private EditText sbd;
+	private ProgressDialog pd;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.sobaodanh);
+        sbd = (EditText) this.findViewById(R.id.EditText01);
+        ImageButton button = (ImageButton)this.findViewById(R.id.ImageButton01);
+        button.setImageResource(R.drawable.seach);        
+        button.setOnClickListener(this); 
+    }
+	@Override
+	public void onClick(View v) {		
+    	Intent intent = new Intent();
+    	intent.setAction("chi_tiet");
+    	intent.putExtra("sbd", sbd.getText().toString());
+    	startActivityForResult(intent, chi_tiet);
+	}
+}
