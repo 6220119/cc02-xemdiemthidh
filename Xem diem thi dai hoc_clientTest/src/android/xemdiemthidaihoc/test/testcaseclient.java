076cb7a9863f7ddfb752dfe1f174498968d43587
cnitgroup <cnitgroup@0f7d6440-41b5-22ce-4b88-147af5e17b3c>
2010-11-16 18:15:11 +0000

    ket thuc unit test cho ca server và client

---
+++ Xem diem thi dai hoc_clientTest/src/android/xemdiemthidaihoc/test/testcaseclient.java
@@ -0,0 +1,15 @@
+package android.xemdiemthidaihoc.test;
+
+import junit.framework.TestCase;
+
+public class testcaseclient extends TestCase {
+
+	public testcaseclient(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
